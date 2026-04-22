90bb0eaea25527c2e3c43cf8fe4f5c91f028bcb2
Joao <gualberto.silvaj@gmail.com>
2026-04-22 18:15:32 -0400

    refactor: Remove rotas de login e cadastro sem proxy do arquivo web.php

--- Tela-LoginProxy/routes/web.php
+++ Tela-LoginProxy/routes/web.php
@@ -6,13 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(MainControler::class)->group(function(){
   Route::get('/', 'showLoginForm')->name('login');
-  Route::get('login-sem-proxy', 'showLoginFormSemProxy')->name('login-sem-proxy');
   Route::get('cadastro','cadastrar')->name('cadastro');
-  Route::get('cadastro-sem-proxy', 'cadastrarSemProxy')->name('cadastro-sem-proxy');
-
-
-  Route::post('cadastro-sem-proxy', 'cadastroSemProxySubmit')->name('cadastroSemProxy.submit');
-  Route::post('/login-sem-proxy', 'loginSemProxySubmit')->name('loginSemProxy.submit');
 
   Route::post('cadastro', 'cadastrarSubmit')->name('cadastrar.submit');
   Route::post('/login','login')->name('login.submit');
